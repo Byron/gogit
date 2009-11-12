6c76be916d1a07cb4e828244a81b5e2601b05412
Sebastian Thiel <byronimo@gmail.com>
2009-11-12 15:54:01 +0100

    sha1 type is there in an initial version including primitive tests ... its a primitive type after all ;)

--- src/git/obj/base.go
+++ src/git/obj/base.go
@@ -1,2 +1,42 @@
 // Implement basic functions shared by all object types
 package obj
+
+import (
+		"strings";
+		"bytes";
+		"encoding/hex";
+		"os";
+		"fmt";
+)
+
+// Type representing a sha1 with 20 bytes 
+type Sha1 [20]byte;
+
+// String converts a Sha1 to a hexadecimal representation
+func (s Sha1) String() string {
+	return hex.EncodeToString(&s)
+}
+
+// Compare returns True if lhs == rhs
+func (s Sha1) Equals( rhs *Sha1 ) bool {
+	return bytes.Compare(&s, rhs) == 0;
+}
+
+
+// Indicates the passed in string had a lenght != 40 bytes
+type InvalidLengthError uint8;
+func (s InvalidLengthError) String() string { return fmt.Sprintf("Invalid Length: %i", uint8(s)) }
+
+
+// FromHex creates a new sha from its hex representation string
+func FromHex(s string) (*Sha1, os.Error ) {
+	if len(s) != 40 {
+		return nil, InvalidLengthError(len(s));
+	}
+	sha := new(Sha1);
+	_, err := hex.Decode(sha, strings.Bytes(s));
+	if err != nil {
+		return nil, err;
+	}
+	return sha, nil;
+}
--- src/git/obj/base_test.go
+++ src/git/obj/base_test.go
@@ -1,7 +1,60 @@
 package obj
 
-import "testing"
+import "testing" 
+import "fmt"
+
+func TestSha1(t *testing.T)  {
+	var s Sha1;
+	_, err := FromHex("00");
+	if err == nil { 
+		t.Error("created Sha1 from hex being too short"); 
+	}
+	
+	// test printing 
+	fmt.Sprint(err);
+	sp, _ := FromHex(s.String()); 
+	if sp == nil || !sp.Equals(&s) {
+		switch sp {
+		case nil: t.Error( "Did not get any sha from ", s.String());
+		default: t.Errorf( "From hex did not produce an equal sha: %v != %v", sp, s  ) 
+		}
+	}
+	
+	// make copy 
+	s2 := *sp;
+	s2.Equals(sp);
+	// t.Fail();
+}
 
 func TestBasics(t *testing.T)  {
 	t.FailNow();
+}
+
+
+
+
+
+
+/* ASSORTED TESTS */
+
+func byteManip(a [10]byte) {
+	a[0] = 2;
+}
+
+func byteManipPointer(a *[10]byte) {
+	a[0] = 3;
+}
+
+func TestArrayPassing(t *testing.T) {
+	var b [10]byte;
+	byteManip(b);
+	if b[0] != 0{
+		t.Error( "bytes passed by reference")
+	}
+	
+	byteManipPointer(&b);
+	if b[0] != 3 {
+		t.Error( "pointer bytes passed by copy" );
+	}
+	
 }
