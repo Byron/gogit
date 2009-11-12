f4ff639addd42c55c1661993de2804dd24713093
Sebastian Thiel <byronimo@gmail.com>
2009-11-12 17:40:14 +0100

    added perf module for performance testing, moved misc tests there so they do not clutter the 'real' tests

---
+++ src/git/perf/misc_test.go
@@ -0,0 +1,106 @@
+package misc
+
+import "testing"
+
+/* ASSORTED TESTS */
+
+func byteManip(a [10]byte) {
+	a[0] = 2;
+}
+
+func byteManipPointer(a *[10]byte) {
+	a[0] = 3;
+}
+
+func TestArrayPassing(t *testing.T) {
+	var b [10]byte;
+	byteManip(b);
+	if b[0] != 0{
+		t.Error( "bytes passed by reference")
+	}
+	
+	byteManipPointer(&b);
+	if b[0] != 3 {
+		t.Error( "pointer bytes passed by copy" );
+	}
+	
+}
+
+
+// type derived from built-in one
+type tenbyte [10]byte
+
+// discover whether instance methods copy their instance when called
+func (b tenbyte) makeChange() {
+	b[0] = 4;
+}
+
+func (bp *tenbyte) makeChangePointer() {
+	bp[0] = 4;
+}
+
+// type derived from struct
+type simple struct {
+	v int8;
+}
+
+func (s simple) makeChange() {
+	s.v = 1;
+}
+
+func (s *simple) makeChangePointer() {
+	s.v = 1;
+}
+
+
+func TestFunctionInvocation(t *testing.T) {
+	// test whether value methods on value types are true copies
+	var tb tenbyte;
+	tb.makeChange();
+	if tb[0] != 0 {
+		t.Error( "value method on stack instance should have copied itself into method" );
+	}
+	
+	// what happens if pointers are used with value methods ? 
+	// be explict here
+	var tbp *tenbyte= new(tenbyte);	
+	tbp.makeChange();
+	if tbp[0] != 0 {
+		t.Error( "value method on heap instance should have copied itself into method" );
+	}
+	
+	// call pointer func on non-pointer
+	tb.makeChangePointer();
+	if tb[0] != 4 {
+		t.Error( "Change did not propagate to stack instance" );
+	}
+	
+	tbp.makeChangePointer();
+	if tbp[0] != 4 {
+		t.Error( "Change did not propagate to heap instance" );
+	}
+	
+	// same for struct type
+	var s simple;
+	s.makeChange();
+	if s.v != 0 {
+		t.Error( "value method on stack instance should have copied itself into method" );
+	}
+	
+	var sp *simple= new(simple);	
+	sp.makeChange();
+	if sp.v != 0 {
+		t.Error( "value method on heap instance should have copied itself into method" );
+	}
+	
+	// call pointer func on non-pointer
+	s.makeChangePointer();
+	if s.v != 1 {
+		t.Error( "Change did not propagate to stack instance" );
+	}
+	
+	sp.makeChangePointer();
+	if sp.v != 1 {
+		t.Error( "Change did not propagate to heap instance" );
+	}
+}
